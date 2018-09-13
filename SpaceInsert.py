842d5e5d3822f33f305736e34df6e4bf5aaefcb4
sludge3000 <9828190+sludge3000@users.noreply.github.com>
2018-09-13 12:45:56 +0200

    Update SpaceInsert.py
    Added input file check and ethernet header padding.

--- SpaceInsert.py
+++ SpaceInsert.py
@@ -8,14 +8,23 @@
 # Please hex stream in data.txt
 # Run SpaceInsert.py in the same directory as data.txt
 
+while True:
+    try:
+        open('data.txt')
+        break
+    except FileNotFoundError:
+            print('data.txt is not present in the working directory')
+            break
+
 
 infile = open('data.txt')
-outfile = open('test.txt', 'r+')
+outfile = open('test.txt', 'w+')
 read_infile = infile.read()
 # print(read_infile)
 # 0123456789ABCDEF
-new_data = ' '.join([read_infile[i:i+2] for i in range(0, len(read_infile), 2)])
-outfile.write(new_data)
-# 23
+new_data = ' '.join(
+    [read_infile[i:i+2] for i in range(0, len(read_infile), 2)]
+    )
+outfile.write('0000 00 00 00 00 00 00 00 00 00 00 00 00 08 00 ' + new_data)
 infile.close()
 outfile.close()
